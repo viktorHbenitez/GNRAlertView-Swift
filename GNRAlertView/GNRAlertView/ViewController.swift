ca760cc73b4ed370ea0d1465a1d93c5c40624c67
viktor <victorhbenitez85@hotmail.com>
2019-08-23 23:45:13 -0500

    init project

---
+++ GNRAlertView/GNRAlertView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GNRAlertView
+//
+//  Created by Victor Hugo Benitez Bosques on 8/23/19.
+//  Copyright © 2019 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
